56c5ace97f8171767ee368c64e4d0d2262c0228e
V4l0v <valentin.sanchez0b@gmail.com>
2025-08-26 21:04:06 -0300

    Commit sin terminar

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,51 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import ar.edu.unlu.poo.labo1.libreria.Libro;
+
+import java.util.*;
+
 public class Artefacto {
+    private String nombre;
+    private int poder;
+    private String tipo;
+
+
+
+    public Artefacto(String nombre,int poder, String tipo)
+    {
+        this.nombre=nombre;
+        this.poder=poder;
+        this.tipo=tipo;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){ //comprueba si es el mismo objeto
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) return false; // comprueba si es vacio u otra clase
+        Artefacto artefacto= (Artefacto) o;
+        return Objects.equals(this.nombre, artefacto.nombre) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
+
+
+
 }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,39 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+    private Set<Artefacto> mis_artefactos = new HashSet<>();
+
+    public void agregarArtefacto(Artefacto artefacto)
+    {
+        mis_artefactos.add(artefacto);
+    }
+
+
+    public Set obtenerArtefactosUnicos()
+    {
+
+        return mis_artefactos;
+    }
+
+    List buscarArtefactosPorTipo(String tipo)
+    {
+        return null;
+    }
+
+    Map<String,Integer> contarArtefactosPorTipo()
+    {
+        return null;
+    }
+
+    Artefacto obtenerArtefactoMasPoderoso()
+    {
+        return null;
+    }
+
+
+
+
+
 }
